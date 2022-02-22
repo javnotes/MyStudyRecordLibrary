1003065949920861bc8ab4ddd417508af469d155
luf <monkeyd.vuffy@gmail.com>
2022-02-22 23:43:49 +0800

    Spring Boot 处理文件上传

--- upload-demo/src/main/java/ltd/newbee/mall/controller/UploadController.java
+++ upload-demo/src/main/java/ltd/newbee/mall/controller/UploadController.java
@@ -1,8 +1,10 @@
 package ltd.newbee.mall.controller;
 
+import ch.qos.logback.core.db.dialect.MySQLDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,11 +20,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 public class UploadController {
     // 文件保存路径为 D 盘下的 upload 文件夹，可以按照自己的习惯来修改。D:\\upload\\
-    // /Users/luf/Documents/upload-demo/
     // 设置的文件保存路径
     private final static String FILE_UPLOAD_PATH = "/Users/luf/Documents/upload-demo/";
 
@@ -37,48 +39,59 @@ public class UploadController {
         if (file.isEmpty()) {
             return "上传失败";
         }
+
         String fileName = file.getOriginalFilename();
-        String suffixName = fileName.substring(fileName.lastIndexOf("."));
-        //生成文件名称通用方法
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));//获取文件后缀名称，从.开始
+
+        //下面是生成文件名称的通用方法
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyymmdd_HHmmss");
         Random r = new Random();
         StringBuilder tempName = new StringBuilder();
         tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
         String newFileName = tempName.toString();
+
+        // 下面进行读取文件
         try {
-            // 保存文件
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(FILE_UPLOAD_PATH + newFileName);
+            Path path = Paths.get(FILE_UPLOAD_PATH + newFileName);//完整路径，包含文件名及后缀
             Files.write(path, bytes);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "上传成功，地址为：/upload/" + newFileName;
+
+        //原文件名称为fileName
+        return "上传成功，原文件名称为：" + fileName + "，上传后的地址为：/upload/" + newFileName;
     }
+
 
     @RequestMapping(value = "/uploadFilesBySameName", method = RequestMethod.POST)
     @ResponseBody
     public String uploadFilesBySameName(@RequestPart MultipartFile[] files) {
         if (files == null || files.length == 0) {
-            return "参数异常";
+            return "上传异常";
         }
         if (files.length > 5) {
-            return "最多上传5个文件";
+            return "最多可一次上传 5 个文件";
         }
+
         String uploadResult = "上传成功，地址为：<br>";
         for (MultipartFile file : files) {
+
             String fileName = file.getOriginalFilename();
             if (StringUtils.isEmpty(fileName)) {
-                //表示无文件信息，跳出当前循环
+                //表示无文件信息，跳出本次循环
                 continue;
             }
+
             String suffixName = fileName.substring(fileName.lastIndexOf("."));
+
             //生成文件名称通用方法
             SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
             Random r = new Random();
             StringBuilder tempName = new StringBuilder();
             tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
             String newFileName = tempName.toString();
+
             try {
                 // 保存文件
                 byte[] bytes = file.getBytes();
@@ -95,29 +108,33 @@ public class UploadController {
     @RequestMapping(value = "/uploadFilesByDifferentName", method = RequestMethod.POST)
     @ResponseBody
     public String uploadFilesByDifferentName(HttpServletRequest httpServletRequest) {
-        List<MultipartFile> multipartFiles = new ArrayList<>(8);
+
+        List<MultipartFile> multipartFiles = new ArrayList<>();
+
         // 如果不是文件上传请求则不处理
         if (!standardServletMultipartResolver.isMultipart(httpServletRequest)) {
             return "请选择文件";
         }
+
         // 将 HttpServletRequest 对象转换为 MultipartHttpServletRequest 对象，之后读取文件
         MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) httpServletRequest;
-        Iterator<String> iter = multiRequest.getFileNames();
+        Iterator<String> iterator = multiRequest.getFileNames();
         int total = 0;
-        while (iter.hasNext()) {
+        while (iterator.hasNext()) {
             if (total > 5) {
-                return "最多上传5个文件";
+                return "最多上传 5 个文件";
             }
             total += 1;
-            MultipartFile file = multiRequest.getFile(iter.next());
+            MultipartFile file = multiRequest.getFile(iterator.next());
             multipartFiles.add(file);
         }
         if (CollectionUtils.isEmpty(multipartFiles)) {
             return "请选择文件";
         }
         if (multipartFiles != null && multipartFiles.size() > 5) {
-            return "最多上传5个文件";
+            return "最多可上传 5 个文件";
         }
+
         String uploadResult = "上传成功，地址为：<br>";
         for (int i = 0; i < multipartFiles.size(); i++) {
             String fileName = multipartFiles.get(i).getOriginalFilename();
@@ -125,6 +142,7 @@ public class UploadController {
                 //表示无文件信息，跳出当前循环
                 continue;
             }
+
             String suffixName = fileName.substring(fileName.lastIndexOf("."));
             //生成文件名称通用方法
             SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
@@ -132,6 +150,7 @@ public class UploadController {
             StringBuilder tempName = new StringBuilder();
             tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
             String newFileName = tempName.toString();
+
             try {
                 // 保存文件
                 byte[] bytes = multipartFiles.get(i).getBytes();
@@ -144,4 +163,5 @@ public class UploadController {
         }
         return uploadResult;
     }
+
 }
